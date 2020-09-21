402b56df7fb404ab70a2ea490600890faa9f95f7
Camilo Castro <camilo@ninjas.cl>
2020-09-21 04:45:25 -0300

    renamed action to gha

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import action
+import gha
 from sys import exit
 from os import getenv
 from chaucha import opreturn
@@ -9,36 +9,36 @@ def main():
     privkey = getenv("INPUT_PRIVKEY")
 
     if not privkey:
-        action.error("Private Key not found")
+        gha.error("Private Key not found")
         exit(-1)
 
     pubkey = getenv("INPUT_PUBKEY")
 
     if not pubkey:
-        action.error("Public Key not found")
+        gha.error("Public Key not found")
         exit(-1)
 
     sendkey = getenv("INPUT_SENDKEY")
 
     if not sendkey:
-        action.error("Send Key not found")
+        gha.error("Send Key not found")
         exit(-1)
 
     message = getenv("INPUT_MESSAGE")
 
     if not message:
-        action.error("Message not found")
+        gha.error("Message not found")
         exit(-1)
 
-    action.debug("Sending the OP_RETURN")
+    gha.debug("Sending the OP_RETURN")
     response = opreturn.send(privkey, pubkey, sendkey, message)
 
     if not response:
-        action.error("Could not send OP_RETURN")
+        gha.error("Could not send OP_RETURN")
         exit(-1)
 
-    action.debug(response)
-    action.output("response", response)
+    gha.debug(response)
+    gha.output("response", response)
     exit(0)
 
 
